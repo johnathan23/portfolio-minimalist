7dd3b2d40aa8b9c7a703e69a92940cbf465173b1
Johnathan Silva <johnathans@apexglobal.co>
2025-07-26 23:01:03 -0500

    Set Github page config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://johnathan23.github.io/portfolio-minimalist',
+  base: '/portfolio-minimalist/',
+});
